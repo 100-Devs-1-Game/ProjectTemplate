1cf5640c4416ea22742994a1de114bbe74447070
Zephilinox <Zephilinox@hotmail.co.uk>
2025-08-29 14:59:19 +0100

    Remove link to doc that doesn't exist and no one used anyway

--- .hooks/helpful_hints.py
+++ .hooks/helpful_hints.py
@@ -7,7 +7,6 @@ import re
 def main():
 
     print("If you get any linting errors, please look at the `gdlintrc` file for the rules to follow.")
-    print("See also https://github.com/100-Devs-1-Game/All-the-Incrementals/blob/main/docs/github_actions.md#my-github-actions-failed-and-i-dont-know-what-to-do-pls-help")
     return 0
 
 if __name__ == "__main__":
